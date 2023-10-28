5592a342b8dd356f0e675ca286dfe3bb918a889a
ExecutiveAV <info@executiveav.com>
2023-10-28 13:10:43 -0400

    backup

--- src/app/components/Uncovering/Uncovering.tsx
+++ src/app/components/Uncovering/Uncovering.tsx
@@ -15,9 +15,13 @@ const Uncovering = () => {
             </header>
             <section className={styles.content}>
                 <Image src={uncoveringHero} className={styles.contentBackgroundImage} alt="Image of a person using a laptop's keyboard used as a background" />
-                <p className={styles.contentText}>
-                    {"Step into a new realm of optimization for your Amazon listings. At first glance, everything might seem in order, but what lies beneath? Our meticulous analysis dives deep, surfacing those elusive issues - from overlooked keywords to visuals that don't quite hit the mark. These aren't just challenges; they're untapped opportunities waiting to be harnessed. Concurrently, we design optimized storefronts that not only captivate your loyal customers but also foster brand allegiance, encouraging them to explore more of what you offer. Partner with us and unlock the full potential of your brand, ensuring you not only compete but lead in the marketplace."}
-                </p>
+                <div className={styles.contentContainer}>
+                    <div className={styles.contentTextContainer} >
+                        <p className={styles.contentText}>
+                            {"Step into a new realm of optimization for your Amazon listings. At first glance, everything might seem in order, but what lies beneath? Our meticulous analysis dives deep, surfacing those elusive issues - from overlooked keywords to visuals that don't quite hit the mark. These aren't just challenges; they're untapped opportunities waiting to be harnessed. Concurrently, we design optimized storefronts that not only captivate your loyal customers but also foster brand allegiance, encouraging them to explore more of what you offer. Partner with us and unlock the full potential of your brand, ensuring you not only compete but lead in the marketplace."}
+                        </p>
+                    </div>
+                </div>
             </section>
         </section>
     );
