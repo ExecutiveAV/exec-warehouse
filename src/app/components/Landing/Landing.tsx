cbd57031f2792299101715113347fdff3794637d
ExecutiveAV <info@executiveav.com>
2023-11-16 23:40:59 -0500

    uncovering

--- src/app/components/Landing/Landing.tsx
+++ src/app/components/Landing/Landing.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { marcellusFont, permanentMarkerFont } from '@/app/fonts';
 
 import HeroImage from '@/assets/imgs/hero.png';
@@ -13,31 +14,34 @@ import Image from 'next/image';
 const Landing = () => {
     return (
         <section className={styles.landing} >
-            <section className={styles.content} >
-                <Image className={styles.hero} src={HeroImage} alt="Hero Image of a neat warehouse" />
-                <section className={styles.cards} >
-                    <section className={`${styles.card} ${marcellusFont.className}`}>
-                        <Image className={styles.cardImage} src={UncoveringLink} alt="Uncovering Link" />
-                        <p className={styles.cardTitle} >UNCOVERING CHALLENGES</p>
-                    </section>
-                    <section className={`${styles.card} ${marcellusFont.className}`}>
-                        <Image className={styles.cardImage} src={AmplifyLink} alt="Amplify Link" />
-                        <p className={styles.cardTitle} >AMPLIFY YOUR BRANDS PRESENCE</p>
-                    </section>
-                    <section className={`${styles.card} ${marcellusFont.className}`}>
-                        <Image className={styles.cardImage} src={ElevateLink} alt="Elevate Link" />
-                        <p className={styles.cardTitle} >ELEVATE WITH EXCLUSIVITY</p>
-                    </section>
-                    <section className={`${styles.card} ${marcellusFont.className}`}>
-                        <Image className={styles.cardImage} src={CraftingLink} alt="Crafting Link" />
-                        <p className={styles.cardTitle} >CRAFTING CONTENT THAT CONNECTS</p>
-                    </section>
+            <Image className={styles.hero} src={HeroImage} alt="Hero Image of a neat warehouse" />
+            <section className={styles.cards} >
+            <section onClick={() => {
+                const element = document.getElementById('uncovering');
+                if (element) {
+                    element.scrollIntoView({ behavior: 'smooth' });
+                }
+            }} className={`${styles.card} ${marcellusFont.className}`}>
+                    <Image className={styles.cardImage} src={UncoveringLink} alt="Uncovering Link" />
+                    <p className={styles.cardTitle} >UNCOVERING CHALLENGES</p>
+                </section>
+                <section className={`${styles.card} ${marcellusFont.className}`}>
+                    <Image className={styles.cardImage} src={AmplifyLink} alt="Amplify Link" />
+                    <p className={styles.cardTitle} >AMPLIFY YOUR BRANDS PRESENCE</p>
+                </section>
+                <section className={`${styles.card} ${marcellusFont.className}`}>
+                    <Image className={styles.cardImage} src={ElevateLink} alt="Elevate Link" />
+                    <p className={styles.cardTitle} >ELEVATE WITH EXCLUSIVITY</p>
+                </section>
+                <section className={`${styles.card} ${marcellusFont.className}`}>
+                    <Image className={styles.cardImage} src={CraftingLink} alt="Crafting Link" />
+                    <p className={styles.cardTitle} >CRAFTING CONTENT THAT CONNECTS</p>
                 </section>
             </section>
-            
             <footer className={styles.footer}>
-                <p className={`${marcellusFont.className} ${styles.footerText}`} >A <span className={`${permanentMarkerFont.className} ${styles.footerTextBold}`} >PARTNERSHIP</span> WITH US</p>
+                <p className={`${marcellusFont.className} ${styles.footerText}`} >A <span className={`${permanentMarkerFont.className} ${styles.footerTextBold}`} >PARTNERSHIP</span> <br />WITH US</p>
             </footer>
+            
         </section>
     );
 };
--- src/app/components/Uncovering/Uncovering.tsx
+++ src/app/components/Uncovering/Uncovering.tsx
@@ -7,16 +7,15 @@ import { loraFont } from '@/app/fonts';
 
 const Uncovering = () => {
     return (
-        <section className={styles.uncovering}>
+        <section id='uncovering' className={styles.uncovering}>                
+            <header className={styles.uncoveringContentHeader}>
+                <h2 className={`${loraFont.className} ${styles.uncoveringContentHeaderTitle}`} >
+                    <span className={styles.uncoveringContentHeaderTitleBold} >UNCOVERING</span><br/> Hidden Challenges
+                </h2>
+            </header>
             <Image src={uncoveringHero} className={styles.uncoveringBackgroundImage} alt="Image of a person using a laptop's keyboard used as a background" />
-            <div className={styles.uncoveringContentContainer}>
-                
-                <header className={styles.uncoveringContentHeader}>
-                    <h2 className={`${loraFont.className} ${styles.uncoveringContentHeaderTitle}`} >
-                        <span className={styles.uncoveringContentHeaderTitleBold} >UNCOVERING</span> Hidden Challenges
-                    </h2>
-                </header>
-                <div className={styles.uncoveringContentTextContainer} >
+            <div className={styles.uncoveringContentTextContainer} >
+                <div className={styles.uncoveringContentTextContainerBackground} >
                     <p className={styles.uncoveringContentText}>
                         {"Explore a new level of Amazon listing optimization. While things may appear fine, our thorough analysis reveals hidden challenges—from missed keywords to off-target visuals. Turn these challenges into opportunities. We craft captivating storefronts that boost customer loyalty and drive exploration. Partner with us to lead in the marketplace."}
                     </p>
